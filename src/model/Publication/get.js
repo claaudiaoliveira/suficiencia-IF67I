30ae6fbc5ba154d603d5b1ab038f7e63e6fbe1cb
Claúdia de Oliveira Ramos <cmplg20@gmail.com>
2022-10-04 16:29:54 -0300

    Adding src/model/Publication/get.js

---
+++ src/model/Publication/get.js
@@ -0,0 +1,11 @@
+const Table = require('../../table/publications');
+const User = require('../../table/users');
+
+module.exports = async (parameters) => {
+  const result = await Table.findByPk(parameters.id, {
+    attributes: ['id', 'name', 'description', 'ingredients', 'photo', 'price'],
+    include: [{ model: User, attributes: ['id', 'name', 'email', 'type'] }],
+  });
+
+  return result || {};
+};
